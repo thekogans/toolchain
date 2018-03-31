b444837b6004f61c24fc82aaef06c62ea7608e66
Boris Kogan <bkogan@logitech.com>
2018-03-30 20:04:44 -0700

    Reorganized the windows shell script.

--- Windows/setenvironment.bat
+++ Windows/setenvironment.bat
@@ -17,6 +17,9 @@ rem
 rem You should have received a copy of the GNU General Public License
 rem along with thekogans_toolchain. If not, see <http://www.gnu.org/licenses/>.
 
+rem This scripts prepares a cygwin bash shell to be used with thekogans.net
+rem development environment and build system using Visual Studio as the compiler.
+
 rem Use this line in mintty.exe Properties dialog box. You'll need to
 rem provide paths where you installed the toolchain and Visual Studio.
 rem The -p and -s arguments position mintty window. You'll need to
@@ -25,36 +28,33 @@ rem out all together and let the system position and size the window.
 
 rem 'path to cygwin bin'\mintty.exe -i /Cygwin-Terminal.ico -p X,Y -s WIDTH,HEIGHT \
 rem -e %comspec% /k 'path to toolchain root'\Windows\setenvironment.bat \
-rem "path to vcvarsall.bat" [x86 | amd64 | ...] [cl18 | cl19 | cl1910 | ...] [bash]
+rem "path to vcvarsall.bat" [x86 | amd64 | ...] [i386 | x86_64 | ...] [cl18 | cl19 | cl1910 | ...]
 
+rem Perform basic sanity checks.
 if Not Exist ""%1"" (
     echo %1 not found.
     goto usage
 )
-
 if "%2" == "" (
     echo Must provide an architecture parameter to configure Visual Studio.
     goto usage
 )
 
-call %1\vcvarsall.bat %2
+rem Let Visual Studio setup it's comand line build environment.
+call %1 %2
 
-@set TOOLCHAIN_OS=Windows
-if "%2" == "x86" (
-    @set TOOLCHAIN_ARCH=i386
-) else (
-    @set TOOLCHAIN_ARCH=x86_64
-)
-@set TOOLCHAIN_COMPILER=%3
+rem Specify thekogans.net toolchain triplet.
+set TOOLCHAIN_OS=Windows
+set TOOLCHAIN_ARCH=%3
+set TOOLCHAIN_COMPILER=%4
 
-if "%4" == "bash" (
-    %4 --login
-)
+rem Finally, run bash on top of the newly created environment.
+bash --login
 
 goto done
 
 :usage
 
-echo usage: %0 "Path to vcvarsall.bat" [x86 | amd64 | ...] [cl18 | cl19 | cl1910 | ...] [bash]
+echo usage: %0 "Path to vcvarsall.bat" [x86 | amd64 | ...] [i386 | x86_64 | ...] [cl18 | cl19 | cl1910 | ...]
 
 :done
